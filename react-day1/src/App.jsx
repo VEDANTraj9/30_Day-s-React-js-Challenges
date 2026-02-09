c4f178fe9f56cbd5639f12ffdfc7631980ba2559
Mani Kant Kumar <manikantraj060@gmail.com>
2026-02-09 15:01:03 +0530

    update

---
+++ react-day1/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello Manikant 👋</h1>
+      <p>Welcome to the 30 Days of React Challenge!</p>
+      <p>Welcome to the 1st Day of the Challenge!</p>
+
+      {/* Addrese */}
+
+      <div className="address">
+        <p>Name: Manikant Kumar</p>
+        <p>Course: B.Tech CSE</p>
+        <p>Goal: Full stack  Developer</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
